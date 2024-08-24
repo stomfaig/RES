7dab69507f92e238ec386cea06116e9fcf093eb8
gergo.stomfai <stomfaig@gmail.com>
2024-08-24 16:29:14 +0100

    working implementation of ArrayBus and TestBus

--- src/bus.rs
+++ src/bus.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub enum ControlSignal {
     MemEnable = 0b0000_0001,
@@ -8,14 +9,15 @@ pub trait Mem {
     fn new() -> Self;
     fn set_address_bus(&mut self, addr: u16);
     fn set_data_bus(&mut self, val: u8);
+    fn get_data_bus(&self) -> u8;
     fn set_control_signal(&mut self, control: ControlSignal, val: bool);
     fn get_control_signal(&self, control: ControlSignal) -> bool;
 }
 
 pub struct ArrayBus {
-    pub address_bus: u16,
-    pub data_bus: u8,
-    pub control_bus: u8,
+    address_bus: u16,
+    data_bus: u8,
+    control_bus: u8,
     data: [u8; 0xffff],
 }
 
@@ -44,13 +46,15 @@ impl Mem for ArrayBus {
 
     fn set_address_bus(&mut self, addr: u16) {
         self.address_bus = addr;
-        self.update();
     }
 
     fn set_data_bus(&mut self, val: u8) {
         self.data_bus = val;
-        self.update();
     }
+
+    fn get_data_bus(&self) -> u8 {
+        self.data_bus
+    }   
 
     fn set_control_signal(&mut self, control: ControlSignal, val: bool) {
         let mask = control as u8;
@@ -64,3 +68,90 @@ impl Mem for ArrayBus {
     }
 }
 
+/// This is a struct that is designed to test the memory access behavior of implementations.
+/// In addition to implementing the 'Mem' trait,  
+pub struct TestBus {
+    address_bus: u16,
+    data_bus: u8,
+    control_bus: u8,
+    read_targets: HashMap<u16, u8>,
+    write_targets: HashMap<u16, u8>,
+}
+
+impl TestBus {
+
+    pub fn set_read_target(&mut self, addr: u16, val: u8) {
+        self.read_targets.insert(addr, val);
+    }
+
+    pub fn set_read_u16_target(&mut self, addr: u16, val: u16) {
+        let low: u8 = (val & 0xff) as u8;
+        let high: u8 = (val >> 8) as u8;
+        self.read_targets.insert(addr, low);
+        self.read_targets.insert(addr + 1, high);
+    }
+
+    pub fn set_write_target(&mut self, addr: u16, val: u8) {
+        self.write_targets.insert(addr, val);
+    }
+}
+
+impl Mem for TestBus {
+    fn new() -> Self {
+        Self {
+            address_bus: 0,
+            data_bus: 0,
+            control_bus: 0,
+            read_targets: HashMap::new(),
+            write_targets: HashMap::new(),
+        }
+    }
+    fn set_address_bus(&mut self, addr: u16) {
+        self.address_bus = addr;
+        
+    }
+    fn set_data_bus(&mut self, val: u8) {
+        self.data_bus = val;
+    }
+    fn get_data_bus(&self) -> u8 {
+        self.data_bus
+    }
+    fn set_control_signal(&mut self, control: ControlSignal, val: bool) {
+        let mask = control as u8;
+        if (val)  { self.control_bus |= mask; }
+        else { self.control_bus &= !mask; }
+        
+        if (!self.get_control_signal(ControlSignal::MemEnable)) { return; }
+
+        if (self.get_control_signal(ControlSignal::AccessMode)) {
+            let result: Option<&u8> = self.read_targets.get(&self.address_bus);
+            self.data_bus = match result {
+                Some(val) => *val,
+                None => panic!("Method trying to read from forbidden memory."),
+            }
+        } else {
+            let result: Option<&u8> = self.write_targets.get(&self.address_bus);
+            match result {
+                Some(val) => {
+                    if (*val != self.data_bus) { panic!("Method trying to write invalid data."); }
+                },
+                None => panic!("Method trying to write to forbidden memory(addr: {:b}, val: {:b})", self.address_bus, self.data_bus),
+            }
+        }
+    }
+
+    fn get_control_signal(&self, control: ControlSignal) -> bool {
+        (self.control_bus & (control as u8)) != 0
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+
+    }
+}
