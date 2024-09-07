41350a3f30bfe28851e4a3eb538aded2fa3f5a3a
gergo.stomfai <stomfaig@gmail.com>
2024-09-07 18:15:58 +0100

    rom reader from file added

--- src/rom.rs
+++ src/rom.rs
@@ -1,9 +1,43 @@
-
+use std::fs;
 
 pub trait Rom {
     fn load(&mut self, raw: &Vec<u8>, trainer: bool) -> Result<(), String>;
     fn prg_read(&self, address: u16) -> u8;
     fn chr_read(&self, address: u16) -> u8;
+}
+
+
+pub fn rom_reader() -> Result<Box<dyn Rom>, String> {
+    let raw: Vec<u8> = match fs::read("./cartridges/nestest.nes") {
+        Ok(raw) => raw,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    if raw.len() < 16 { return Err(String::from("Invalid INES header...")) }
+
+    let prg_rom_chunks = raw[4];
+    let chr_rom_chunks = raw[5];
+    let trainer: bool = raw[6] & 0b100 != 0;
+    let rom_mapper = ((raw[6] & 0b1111_0000) >> 4) | (raw[7] & 0b1111_0000);
+    let ines_version = if (raw[7] & 0b1100 >> 1 == 0b10) { 2 } else { 1 };
+
+    let mut rom: Box<dyn Rom> = match rom_mapper {
+        0 => {
+            match prg_rom_chunks {
+                1 => Box::new(NROM_128::new()),
+                2 => Box::new(NROM_256::new()),
+                _ => return Err(format!("NROM does not support {:?} prg chunks!", prg_rom_chunks)),
+            }
+        },
+        _ => {
+            return Err(String::from(format!("INES rom mapper {:?} is not supported.", rom_mapper)))
+        }
+    };
+
+    match rom.load(&raw, trainer) {
+        Ok(()) => Ok(rom),
+        Err(e) => Err(e),
+    }
 }
 
 pub struct NROM_128 {
