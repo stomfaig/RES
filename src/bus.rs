5dab666a42489a9e891d8cb19b050036d2275e25
gergo.stomfai <stomfaig@gmail.com>
2024-09-07 18:05:39 +0100

    RomBus working implementation

--- src/bus.rs
+++ src/bus.rs
@@ -159,18 +159,71 @@ pub struct RomBus<T: Rom> {
     address_bus: u16,
     data_bus: u8,
     control_bus: u8,
-    data: [u8; 0xffff],
-    rom: Option<T>,
+    data: [u8; 0x0800],
+    rom: Box<dyn Rom>,
 }
 
-impl<T: Rom> Mem for RomBus<T> {
+impl RomBus {
+    
+    fn update(&mut self) {
+        if (!self.get_control_signal(ControlSignal::MemEnable)) { return; }
+
+        if (self.get_control_signal(ControlSignal::AccessMode)) { // read from mem
+            match self.address_bus {
+                0..=0x1fff => {
+                    let addr: u16 = self.address_bus % 0x0800;
+                    self.data_bus = self.data[addr as usize];
+                },
+                0x2000..=0x3fff => {
+                    let ppu_reg = self.address_bus % 0x0008;
+                    
+                }, // ppu registers
+                0x4000..=0x4017 => {}, // apu and io registers
+                0x4018..=0x401f => {}, // apu and io func normally disabled.
+                0x6000..=0x7fff => {
+
+                }, // Catridge RAM when present
+                0x8000..=0xffff => {
+                    self.data_bus = (*self.rom).prg_read(self.address_bus);
+                },
+                _ => {todo!("what happens in this range?")},
+            };
+        } else {
+            match self.address_bus {
+                0..=0x1fff => {
+                    let addr: u16 = self.address_bus % 0x0800;
+                    self.data[addr as usize] = self.data_bus;
+                },
+                0x2000..=0x3fff => {
+                    let ppu_reg = self.address_bus % 0x0008;
+                    
+                }, // ppu registers
+                0x4000..=0x4017 => {}, // apu and io registers
+                0x4018..=0x401f => {}, // apu and io func normally disabled.
+                0x6000..=0x7fff => {
+
+                }, // Catridge RAM when present
+                0x8000..=0xffff => {
+                    panic!("Program trying to write to ROM.")
+                },
+                _ => {todo!("what happens in this range?")},
+            }
+        }
+    }
+
+    pub fn set_rom(&mut self, rom: Box<dyn Rom>) {
+        self.rom = rom;
+    }
+}
+
+impl Mem for RomBus {
     fn new() -> Self {
         Self {
             address_bus : 0,
             data_bus : 0,
             control_bus : 0,
-            data : [0; 0xffff],
-            rom : None,
+            data : [0; 0x0800],
+            rom : Box::new(EmptyRom::new()),
         }
     }
 
@@ -187,7 +240,10 @@ impl<T: Rom> Mem for RomBus<T> {
     }   
 
     fn set_control_signal(&mut self, control: ControlSignal, val: bool) {
-        
+        if val { self.control_bus |= (control as u8); }
+        else { self.control_bus &= !(control as u8); }
+
+        self.update();
     }
 
     fn get_control_signal(&self, control: ControlSignal) -> bool {
